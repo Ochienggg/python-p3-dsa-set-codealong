aa4ae6f00e67ceadd4a753bbf78679635dd335d3
ochieng <ochieng.oduor@student.moringaschool.com>
2025-08-13 07:22:51 +0300

    done

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,21 @@
 class MySet:
+    def __init__(self, elements=None):
+        if elements is None:
+            elements = []
+        self.dictionary = {}
+        for element in elements:
+            self.dictionary[element] = True
 
-    pass
+    def has(self, element):
+        return element in self.dictionary
+
+    def add(self, element):
+        self.dictionary[element] = True
+        return self
+
+    def delete(self, element):
+        self.dictionary.pop(element, None)
+        return self
+
+    def size(self):
+        return len(self.dictionary)
